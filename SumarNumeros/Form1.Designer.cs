c72bd9cd1101a818b3b008ca9a16b643559a2fe1
stephanie hernandez <ah939278@gmail.com>
2021-11-17 11:24:07 -0300

    Diseño de pantalla de sumar numeros

--- SumarNumeros/Form1.Designer.cs
+++ SumarNumeros/Form1.Designer.cs
@@ -29,13 +29,113 @@ namespace SumarNumeros
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.titulo = new System.Windows.Forms.Label();
+            this.n1 = new System.Windows.Forms.Label();
+            this.n2 = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(94, 86);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 0;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(311, 86);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 1;
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Location = new System.Drawing.Point(228, 9);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(112, 13);
+            this.titulo.TabIndex = 2;
+            this.titulo.Text = "Suma de dos numeros";
+            this.titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // n1
+            // 
+            this.n1.AutoSize = true;
+            this.n1.Location = new System.Drawing.Point(12, 86);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(53, 13);
+            this.n1.TabIndex = 3;
+            this.n1.Text = "Numero 1";
+            // 
+            // n2
+            // 
+            this.n2.AutoSize = true;
+            this.n2.Location = new System.Drawing.Point(228, 89);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(53, 13);
+            this.n2.TabIndex = 4;
+            this.n2.Text = "Numero 2";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(439, 84);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 5;
+            this.btnSumar.Text = "Calcular";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(12, 135);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(31, 13);
+            this.labelTotal.TabIndex = 6;
+            this.labelTotal.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(94, 128);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(556, 217);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Label n1;
+        private System.Windows.Forms.Label n2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.TextBox txtTotal;
     }
 }
 
